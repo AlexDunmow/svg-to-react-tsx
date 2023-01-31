00d35512ddd9d55e342965066f58c7afd08ad85e
AlexDunmow <alex@alexdunmow.com>
2023-02-01 00:40:38 +0800

    added --prefix and --suffix

--- README.md
+++ README.md
@@ -11,8 +11,17 @@ npm install -g svg-to-react-tsx
 ## Usage
 
 ```bash
-svg-to-react-tsx <Folder path of SVGs> [TSX Output Folder] [--lazy]
+svg-to-react-tsx <Folder path of SVGs> [TSX Output Folder] [--lazy] [--suffix="Suffix"] []
 ```
+
+## Options
+
+### --lazy
+Use lazy importing for your index.ts.
+### --prefix=Prefix
+Prefix output filename and component prefix (default is "Icon").
+### --suffix=Suffix
+Suffix output filename and component suffix.
 
 ## Parameters
 
@@ -22,9 +31,21 @@ svg-to-react-tsx <Folder path of SVGs> [TSX Output Folder] [--lazy]
 
 ## Example
 
+To convert all the SVG files in the folder svgs and save the resulting TSX files to the folder components, use the following command:
+
 ```bash
-svg-to-react-tsx ./svgs ./tsx-output --lazy
+svg-to-react-tsx svgs components
 ```
+
+To convert all the SVG files in the folder svgs, save the resulting TSX files to the same folder, and give the components a suffix of "SVG", use the following command:
+
+```bash
+svg-to-react-tsx svgs --suffix="SVG" --lazy
+```
+
+## Requirements
+
+This script requires Node.js and SVGR to be installed on your system. Please refer to the official documentation for installation instructions.
 
 ## Contributing
 
--- index.js
+++ index.js
@@ -6,9 +6,11 @@ const SVGR = require("@svgr/core");
 
 function help() {
   console.log(
-    "# svg-to-react-tsx <Folder path of SVGs> [TSX Output Folder (optional)] [--lazy]"
+    "# svg-to-react-tsx <Folder path of SVGs> [TSX Output Folder] [--lazy]"
   );
   console.log("--lazy flag to use lazy importing for your index.ts");
+  console.log("--prefix=Prefix output filename and component prefix");
+  console.log("--suffix=Suffix output filename and component suffix");
 }
 
 if (process.argv.length === 2) {
@@ -18,40 +20,59 @@ if (process.argv.length === 2) {
 }
 
 const flags = [];
-const fpath = { in: null, out: null };
-const paths = process.argv.forEach((p, index) => {
-  if (index > 1 && !p.startsWith("-")) {
-    if (fpath.in) {
-      if (fpath.out) {
+const opts = { in: null, out: null, prefix: "Icon", suffix: "", lazy: false };
+process.argv.forEach((p, index) => {
+  if (index > 1 && !p.startsWith("--")) {
+    if (opts.in) {
+      if (opts.out) {
         console.log("Unexpected argument.");
         process.exit(1);
       }
-      fpath, (out = p);
+      opts, (out = p);
     } else {
-      fpath.in = p;
+      opts.in = p;
     }
-  } else if (p.startsWith("-")) {
-    flags.push(p);
+  } else if (p.startsWith("--")) {
+    if (p === "--help") {
+      help();
+      process.exit(0);
+    } else if (p.startsWith("--suffix") || p.startsWith("--prefix")) {
+      const flagParts = p.split("=");
+      if (flagParts.length !== 2) {
+        console.log(
+          "Error! Flag needs to formatted as such: " + flagParts[0] + "=Example"
+        );
+        help();
+        process.exit(1);
+      }
+      opts[flagParts[0].replace("--", "")] = flagParts[1];
+    } else if (p === "--lazy") {
+      opts.lazy = true;
+    }
   }
 });
 
-if (!fpath.in) {
-  console.log("No path.");
+if (!opts.in) {
+  console.log("Error! No path provided.");
   help();
   process.exit(1);
 }
 
-if (!path.out) fpath.out = fpath.in;
+if (!path.out) opts.out = opts.in;
 
-function formatFileName(filename) {
+function formatFileName(filename, prefix, suffix) {
   const s = path.basename(filename, path.extname(filename));
-  return s
-    .replace(/^[a-z]|[^a-zA-Z0-9]+[a-z]/g, (match, index) => {
-      return index === 0
-        ? match.toUpperCase()
-        : match.toUpperCase().replace(" ", "");
-    })
-    .replace(/[^a-zA-Z0-9]+/g, "");
+  return (
+    prefix +
+    s
+      .replace(/^[a-z]|[^a-zA-Z0-9]+[a-z]/g, (match, index) => {
+        return index === 0
+          ? match.toUpperCase()
+          : match.toUpperCase().replace(" ", "");
+      })
+      .replace(/[^a-zA-Z0-9]+/g, "") +
+    suffix
+  );
 }
 
 if (flags.indexOf("--help") > -1) {
@@ -60,7 +81,7 @@ if (flags.indexOf("--help") > -1) {
 }
 
 // Loop through all the files in the temp directory
-fs.readdir(fpath.in, function (err, files) {
+fs.readdir(opts.in, function (err, files) {
   if (err) {
     console.error("Could not list the directory.", err);
     process.exit(1);
@@ -75,7 +96,7 @@ fs.readdir(fpath.in, function (err, files) {
   files.forEach(async function (file, index) {
     // Make one pass and make the file complete
 
-    const newFN = formatFileName(file);
+    const newFN = formatFileName(file, opts.prefix, opts.suffix);
     keys.push(newFN);
 
     process.stdout.write(
@@ -90,49 +111,45 @@ fs.readdir(fpath.in, function (err, files) {
         ".tsx                  \r"
     );
 
-    fs.readFile(
-      path.join(fpath.in, file),
-      "utf8",
-      async (err, fileContents) => {
-        // TODO: Handle error case here
+    fs.readFile(path.join(opts.in, file), "utf8", async (err, fileContents) => {
+      // TODO: Handle error case here
 
-        if (err) {
-          console.error("Could not open the file.", err);
-          process.exit(1);
-        }
-
-        const jsCode = await SVGR.transform(
-          fileContents,
-          { icon: true, typescript: true, prettier: true },
-          { componentName: newFN }
-        );
-
-        fs.writeFile(
-          path.join(fpath.out, newFN + ".tsx"),
-          jsCode,
-          "utf8",
-          (err) => {
-            if (err) {
-              console.error("Could not write TSX file.", err);
-              process.exit(1);
-            }
-          }
-        );
+      if (err) {
+        console.error("Could not open the file.", err);
+        process.exit(1);
       }
-    );
+
+      const jsCode = await SVGR.transform(
+        fileContents,
+        { icon: true, typescript: true, prettier: true },
+        { componentName: newFN }
+      );
+
+      fs.writeFile(
+        path.join(opts.out, newFN + ".tsx"),
+        jsCode,
+        "utf8",
+        (err) => {
+          if (err) {
+            console.error("Could not write TSX file.", err);
+            process.exit(1);
+          }
+        }
+      );
+    });
   });
   tsIndex += keys
     .map((key, i) => (i === 0 ? '"' + key + '"' : ' | "' + key + '"'))
     .join("");
   tsIndex += "\n\n";
   keys.forEach((key) => {
-    if (flags.indexOf("--lazy") > -1) {
+    if (opts.lazy) {
       tsIndex += `export const ${key} = React.lazy(() => import('./${key}'));\n`;
     } else {
       tsIndex += `export const ${key} = import('./${key}');\n`;
     }
   });
-  fs.writeFile(path.join(fpath.out, "index.ts"), tsIndex, "utf8", (err) => {
+  fs.writeFile(path.join(opts.out, "index.ts"), tsIndex, "utf8", (err) => {
     if (err) {
       console.error("Could not write index file.", err);
       process.exit(1);
